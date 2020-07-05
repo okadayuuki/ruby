d93e55d7d33aece433c9ddfadd2d0eee977e8447
@okadayuuki <oy19950908@gmail.com>
2020-07-05 14:57:59 +0900

    start2-8

---
+++ lesson6.rb
@@ -0,0 +1,16 @@
+apple =110
+if apple >= 100
+	puts"リンゴの値段は１００円以上です。"
+end
+
+if apple <= 100
+	puts "リンゴの値段は１００円以下です。"
+end
+total_price = 100
+if total_price > 100
+	puts "みかんを購入。所持金に余りあり"
+elsif total_price =100
+	puts "みかんを購入。所持金は０円"
+else total_price <100
+	puts "みかんを購入することができません。"
+end
